3e6d428743f8820800da5e44a3dff2e1d5e0a06e
Shaiful Islam <69207817+shaifulkingdom@users.noreply.github.com>
2023-09-24 02:19:13 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,66 @@
+//=================================================================================//
+//                                banner-part-start
+//=================================================================================//
+$('.banner-part').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplayspeed: 500,
+    arrows: true,
+    prevArrow: '<i class="fa fa-angle-left prev" aria-hidden="true"></i>',
+    nextArrow: '<i class="fa fa-angle-right next" aria-hidden="true"></i>',
+    fade: false,
+    responsive: [{
+            breakpoint: 991.98,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+            }
+        },
+        {
+            breakpoint: 767.98,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 575.98,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+//=================================================================================//
+//                                banner-part-end
+//=================================================================================//
+
+//=================================================================================//
+//                                lock-part-end
+//=================================================================================//
+
+document.onkeydown = function (e) {
+    if (event.keyCode == 123) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+    }
+    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+    }
+}
+
+//=================================================================================//
+//                                lock-part-end
+//=================================================================================//
