897bafdc349049161061c3353ad117dcc27c5dae
Shaiful Islam <shaifulkingdom@gmail.com>
2023-10-12 12:03:35 +0600

    Update it-officer.js

--- js/template/it-officer.js
+++ js/template/it-officer.js
@@ -14,7 +14,10 @@ fetch("js/json/it-officer.json")
 				<td class="image"><img src="${person.image}" alt="image"></td>
 				<td class="name">${person.name}</td>
                 <td class="designation">${person.designation}</td>
-                <td class="phone"><a href="tel:${person.phone}">${person.phone}</a></td>
+                <td class="phone email">
+                    <a href="tel:${person.phone}">${person.phone}</a>
+                    <a href="mailto:${person.email}">${person.email}</a>
+                </td>
                 <td class="email"><a href="mailto:${person.email}">${person.email}</a></td>
 			</tr>
 		`;
